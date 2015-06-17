9a1ded39f7ddc2ff468d4648069c45e4a0068081
JonathanYde <jonathan.yde@biomed.au.dk>
2015-06-17 16:12:14 +0200

    copy shiny folder

---
+++ StudentMaterials/tutorialKnitr_JonathanYde/CopyOfshinyex2/server.R
@@ -0,0 +1,24 @@
+#### Server.R ########
+library(shiny)
+library(ggplot2)
+library(googleVis)
+data("mpg")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$plotOne <- renderPlot({
+    selectedData <- subset(Fruits, Fruits$Year==input$yearInput)
+    p <- ggplot(selectedData, aes(x=Sales, y=Expenses, colour=Fruit))+
+      geom_point(aes(size=Profit))
+    print(p)
+  })
+  
+  
+  output$plotTwo <- renderPlot({
+    p <- ggplot(Fruits, aes_string(x="Fruit", y=input$var, colour="Location"))+
+      geom_boxplot()
+    print(p)
+  })
+  
+})
